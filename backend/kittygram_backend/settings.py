4ba176f172627fc667e7a58aa80066c48e76d7b0
sadsadhead <sadsadhead@users.noreply.github.com>
2024-04-28 22:01:56 +0300

    refresh

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,14 +1,12 @@
 # flake8: noqa
 import os
 from pathlib import Path
-from dotenv import load_dotenv
 
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY', 'it_is_my_secret_key')
+SECRET_KEY = os.getenv('SECRET_KEY', 'shhh')
 
 DEBUG = os.getenv('DEBUG', True)
 
